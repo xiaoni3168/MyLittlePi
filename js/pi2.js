63a2414f37feb6c63e36421d59a6832c820c19f2
Xiaoni <xiaoni3168@163.com>
2015-06-14 00:35:37 +0800

    bdy

--- js/pi2.js
+++ js/pi2.js
@@ -3,7 +3,6 @@ var getBDY = function() {
 	var target = $(event.target);
 	var href = target.parent().find('a').get(0).href;
 	$.getJSON('./js/bdy.json', function(data) {
-		console.log('run first');
-		console.log(data);
+		console.log(data[href]);
 	});
 }
