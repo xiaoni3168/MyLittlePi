77e6ad30198e040ebc44d57b494b4ce05a9b5ccb
xiaoni3168 <nirui3168@163.com>
2015-06-14 00:16:49 +0800

    Update pi2.js
    change

--- js/pi2.js
+++ js/pi2.js
@@ -2,7 +2,7 @@ console.log('this is a javascript file of pi2.html');
 var getBDY = function() {
 	var target = $(event.target);
 	var href = target.parent().find('a').get(0).href;
-	$.getJSON('js/bdy.json', function(data) {
+	$.getJSON('../js/bdy.json', function(data) {
 		console.log('run first');
 		console.log(data);
 	});
