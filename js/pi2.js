d2eb5b3ed8352506c32777d25096fd3aa1733c3a
Xiaoni <xiaoni3168@163.com>
2015-06-14 01:15:09 +0800

    code

--- js/pi2.js
+++ js/pi2.js
@@ -3,6 +3,45 @@ var getBDY = function() {
 	var target = $(event.target);
 	var href = target.parent().find('a').get(0).href;
 	$.getJSON('./js/bdy.json', function(data) {
-		console.log(data[href]);
+		var body = document.body;
+		var bdy = document.createElement('div');
+		var bdyClose = document.createElement('div');
+		var code = document.createElement('div');
+		bdyClose.innerHTML = '&times;';
+		code.innerHTML = data[href];
+		$(bdyClose).click(function() {
+			$(bdy).addClass('fadeOutDown');
+			setTimeout(function() {
+				bdy.remove();
+			}, 1000);
+		});
+		$(bdy).addClass('animated rubberBand');
+		$(bdy).addClass('bdy');
+		$(bdyClose).addClass('bdy-close');
+		$(code).addClass('bdy-code');
+		bdy.appendChild(bdyClose);
+		bdy.appendChild(code);
+		body.appendChild(bdy);
 	});
+}
+var testAni = function() {
+	var body = document.body;
+	var bdy = document.createElement('div');
+	var bdyClose = document.createElement('div');
+	var code = document.createElement('div');
+	bdyClose.innerHTML = '&times;';
+	code.innerHTML = 'hc78';
+	$(bdyClose).click(function() {
+		$(bdy).addClass('fadeOutDown');
+		setTimeout(function() {
+			bdy.remove();
+		}, 1000);
+	});
+	$(bdy).addClass('animated rubberBand');
+	$(bdy).addClass('bdy');
+	$(bdyClose).addClass('bdy-close');
+	$(code).addClass('bdy-code');
+	bdy.appendChild(bdyClose);
+	bdy.appendChild(code);
+	body.appendChild(bdy);
 }
